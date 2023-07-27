6228ef5fb8b27cfb3e6ed1bcd6312fdbeb6cfbbd
sedorfe <bazigafabregas@gmail.com>
2023-07-27 09:57:27 -0700

    5-main.c

---
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char str[] = "Look up!\n";
+    char *ptr;
+
+    ptr = string_toupper(str);
+    printf("%s", ptr);
+    printf("%s", str);
+    return (0);
+}
+julien@ubuntu:~/0x06$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 5-main.c 5-string_toupper.c -o 5-string_toupper
+julien@ubuntu:~/0x06$ ./5-string_toupper 
+LOOK UP!
+LOOK UP!
+julien@ubuntu:~/0x06$ 
+Repo:
+
+GitHub repository: alx-low_level_programming
+Directory: 0x06-pointers_arrays_strings
+File: 5-string_toupper.c
+   
+6. Expect the best. Prepare for the worst. Capitalize on what comes
+mandatory
+Write a function that capitalizes all words of a string.
+
+Prototype: char *cap_string(char *);
+Separators of words: space, tabulation, new line, ,, ;, ., !, ?, ", (, ), {, and }
+julien@ubuntu:~/0x06$ cat 6-main.c
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+    char *ptr;
+
+    ptr = cap_string(str);
+    printf("%s", ptr);
+    printf("%s", str);
+    return (0);
+}
